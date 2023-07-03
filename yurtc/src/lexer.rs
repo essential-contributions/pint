84c9fe953919ff6706e373fa8c1bc43dd0e143fe
Ian Cash <iancashdeveloper@gmail.com>
2023-07-03 12:30:00 -0500

    Test all ops

--- yurtc/src/lexer.rs
+++ yurtc/src/lexer.rs
@@ -188,8 +188,11 @@ fn operators() {
     assert_eq!(lex_one_success("!"), Token::Not);
     assert_eq!(lex_one_success("+"), Token::Plus);
     assert_eq!(lex_one_success("-"), Token::Minus);
+    assert_eq!(lex_one_success("*"), Token::Star);
     assert_eq!(lex_one_success("/"), Token::Div);
     assert_eq!(lex_one_success("%"), Token::Mod);
+    assert_eq!(lex_one_success(">"), Token::Gt);
+    assert_eq!(lex_one_success("<"), Token::Lt);
     assert_eq!(lex_one_success("<="), Token::LtEq);
     assert_eq!(lex_one_success(">="), Token::GtEq);
     assert_eq!(lex_one_success("=="), Token::EqEq);
