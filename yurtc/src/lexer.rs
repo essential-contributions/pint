d2dbfd94ac9445e5928ee0e3aca897c8148d9056
Toby Hutton <toby@grusly.com>
2023-07-11 17:21:09 +1000

    Use a nested tokeniser to parse the characters within a string literal.

--- yurtc/src/lexer.rs
+++ yurtc/src/lexer.rs
@@ -94,7 +94,12 @@ pub(super) enum Token<'sc> {
     IntLiteral(&'sc str),
     #[regex(
         r#""([^"\\]|\\(x[0-9a-fA-F]{2}|[nt"]|\\|\n))*""#,
-        process_string_literal
+        |lex| {
+            StringLiteralChar::lexer(lex.slice())
+                .map(|c| c.map(char::from))
+                .collect::<Result<String, _>>()
+                .unwrap()
+        }
     )]
     StringLiteral(String),
 
@@ -170,11 +175,6 @@ impl<'sc> fmt::Display for Token<'sc> {
     }
 }
 
-#[cfg(test)]
-fn check(actual: &str, expect: expect_test::Expect) {
-    expect.assert_eq(actual);
-}
-
 /// Lex a stream of characters. Return a list of discovered tokens and a list of errors encountered
 /// along the way.
 pub(super) fn lex(src: &str) -> (Vec<(Token, Span)>, Vec<CompileError>) {
@@ -186,51 +186,67 @@ pub(super) fn lex(src: &str) -> (Vec<(Token, Span)>, Vec<CompileError>) {
         })
 }
 
-fn process_string_literal<'sc>(lex: &mut logos::Lexer<'sc, Token<'sc>>) -> String {
-    let raw_string = lex.slice().to_string();
-    let mut final_string = String::new();
-    let mut chars = raw_string.chars().peekable();
+#[derive(Clone, Debug, Eq, Hash, Logos, PartialEq, Ord, PartialOrd)]
+#[logos(error = LexError)]
+enum StringLiteralChar {
+    // The lex.slice() is the whole matched '\xDD'.  It's easy to create an invalid character this
+    // way as far as Rust is concerned, so if it fails we currently return 0.  Supporting UTF8
+    // properly or treating Yurt strings as `[u8]` instead of `String` is a TODO issue.
+    #[regex(r"\\x[0-9a-fA-F]{2}",
+        |lex| {
+            char::from_u32(
+                lex.slice()
+                .chars()
+                .skip(2)
+                .fold(0, |n, c| n * 16 + c.to_digit(16).unwrap()),
+            )
+            .unwrap_or('\x00')
+        }
+    )]
+    HexEscape(char),
 
-    while let Some(c) = chars.next() {
-        match c {
-            '\\' => {
-                if let Some(&next_char) = chars.peek() {
-                    match next_char {
-                        'n' => {
-                            final_string.push('\n');
-                            chars.next();
-                        }
-                        't' => {
-                            final_string.push('\t');
-                            chars.next();
-                        }
-                        '\\' => {
-                            final_string.push('\\');
-                            chars.next();
-                        }
-                        '"' => {
-                            final_string.push('"');
-                            chars.next();
-                        }
-                        '\n' => {
-                            chars.next();
-                            while let Some(&next_char) = chars.peek() {
-                                if next_char.is_whitespace() {
-                                    chars.next();
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                        _ => final_string.push(c),
-                    }
-                }
+    #[token(r"\n", |_| '\n')]
+    Newline(char),
+
+    #[token(r"\t", |_| '\t')]
+    Tab(char),
+
+    #[token(r#"\""#, |_| '\"')]
+    DoubleQuote(char),
+
+    #[token(r"\\", |_| '\\')]
+    Backslash(char),
+
+    #[regex(r"\\\n[ \t]*", logos::skip)]
+    JoinNewline,
+
+    #[token(r#"""#, logos::skip)]
+    Delimiter,
+
+    #[regex(r#"[^"\\]"#, |lex| lex.slice().chars().next().unwrap())]
+    Any(char),
+}
+
+impl From<StringLiteralChar> for char {
+    fn from(value: StringLiteralChar) -> Self {
+        match value {
+            StringLiteralChar::HexEscape(c)
+            | StringLiteralChar::Newline(c)
+            | StringLiteralChar::Tab(c)
+            | StringLiteralChar::DoubleQuote(c)
+            | StringLiteralChar::Backslash(c)
+            | StringLiteralChar::Any(c) => c,
+
+            StringLiteralChar::JoinNewline | StringLiteralChar::Delimiter => {
+                unreachable!("Should be skipped by the tokenizer.")
             }
-            '"' => {}
-            _ => final_string.push(c),
         }
     }
-    final_string
+}
+
+#[cfg(test)]
+fn check(actual: &str, expect: expect_test::Expect) {
+    expect.assert_eq(actual);
 }
 
 #[cfg(test)]
@@ -313,8 +329,16 @@ fn strings() {
         Token::StringLiteral("Hello, \" world\"!".to_string())
     );
     assert_eq!(
-        lex_one_success(r#""Hello, \\ world!""#),
+        lex_one_success("\"Hello, \\\\ world!\""),
         Token::StringLiteral("Hello, \\ world!".to_string())
+    );
+    assert_eq!(
+        lex_one_success("\"x\\x41\\x2b\\x7ab\""),
+        Token::StringLiteral("xA+zb".to_string())
+    );
+    assert_eq!(
+        lex_one_success("\"aha\\x0a\\x0d\\x09\""),
+        Token::StringLiteral("aha\n\r\t".to_string())
     );
 }
 
