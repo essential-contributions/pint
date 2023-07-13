1b83e432c2676ea3b1500770d6a4dcc96e38497e
Ian Cash <iancashdeveloper@gmail.com>
2023-07-13 10:07:29 -0500

    add +1 test

--- yurtc/src/lexer/tests.rs
+++ yurtc/src/lexer/tests.rs
@@ -50,6 +50,7 @@ fn reals() {
 fn ints() {
     assert_eq!(lex_one_success("1"), Token::IntLiteral("1"));
     assert_eq!(lex_one_success("-1"), Token::IntLiteral("-1"));
+    assert_eq!(lex_one_success("+1"), Token::IntLiteral("+1"));
     assert_eq!(lex_one_success("0030"), Token::IntLiteral("0030"));
     assert_eq!(lex_one_success("0x333"), Token::IntLiteral("0x333"));
     assert_eq!(lex_one_success("0b1010"), Token::IntLiteral("0b1010"));
