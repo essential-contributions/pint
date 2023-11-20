e2eb3cf69fa3d55ad98a6450d0bec4494e4bdf8f
Ian Cash <iwc123@gmail.com>
2023-11-20 08:19:03 -0600

    Add more range tests

--- yurtfmt/src/ast/tests.rs
+++ yurtfmt/src/ast/tests.rs
@@ -742,7 +742,15 @@ y ||    42 in   z"
 #[test]
 fn range_expressions() {
     check(
-        &run_formatter!(expr(), "1..2"),
+        &run_formatter!(range(expr()), "1..2"),
         expect_test::expect![[r#"1..2"#]],
+    );
+    check(
+        &run_formatter!(range(expr()), "1+2..3+4"),
+        expect_test::expect!["1 + 2..3 + 4"],
+    );
+    check(
+        &run_formatter!(range(expr()), "1.1..2.2e3"),
+        expect_test::expect!["1.1..2.2e3"],
     );
 }
