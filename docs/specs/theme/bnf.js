61281496f6ea4564f8fad07c88bfa5a6207697d6
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2023-11-23 13:55:33 -0500

    Yurt documentation

---
+++ docs/specs/theme/bnf.js
@@ -0,0 +1,31 @@
+hljs.registerLanguage("bnf", (hljs) => ({
+  name: "BNF",
+  contains: [
+    // Attribute
+    {
+      className: "attribute",
+      begin: /</,
+      end: />/,
+    },
+    // Specific
+    {
+      begin: /::=/,
+      starts: {
+        end: /$/,
+        contains: [
+          {
+            begin: /</,
+            end: />/,
+          },
+          // Common
+          hljs.C_LINE_COMMENT_MODE,
+          hljs.C_BLOCK_COMMENT_MODE,
+          hljs.APOS_STRING_MODE,
+          hljs.QUOTE_STRING_MODE,
+        ],
+      },
+    },
+  ],
+}));
+
+hljs.initHighlightingOnLoad();
