af6a482df30be662caf64e552c11463b4ef78c94
Ian Cash <iwc123@gmail.com>
2023-12-05 11:36:29 -0600

    Unformat tests

--- yurtfmt/src/ast/tests.rs
+++ yurtfmt/src/ast/tests.rs
@@ -924,31 +924,53 @@ fn if_exprs() {
         }"#]],
     );
 }
-
 #[test]
 fn cond_exprs() {
-    // check(
-    //     &run_formatter!(cond_expr(expr()), "cond { else => { a } }"),
-    //     expect_test::expect![[r#"
-    //     cond { else => {
-    //             a
-    //         }
-    //     }"#]],
-    // );
     check(
-        &run_formatter!(cond_expr(expr()), "cond { else => a, }"),
+        &run_formatter!(cond_expr(expr()), "cond {   else => {  a }  }"),
+        expect_test::expect![[r#"
+        cond { else => {
+            a
+        } }"#]],
+    );
+    check(
+        &run_formatter!(
+            cond_expr(expr()),
+            "cond 
+            {  
+    else => a
+    , }"
+        ),
         expect_test::expect![[r#"cond { else => a, }"#]],
     );
     check(
-        &run_formatter!(cond_expr(expr()), "cond { a => b, else => c }"),
+        &run_formatter!(
+            cond_expr(expr()),
+            "cond {
+            
+              a =>
+               b,  
+        else => c }"
+        ),
         expect_test::expect![[r#"cond { a => b, else => c }"#]],
     );
-    // check(
-    //     &run_formatter!(cond_expr(expr()), "cond { a => { b }, else => c, }"),
-    //     expect_test::expect![[r#"cond { a => { b }, else => c, }"#]],
-    // );
-    // check(
-    //     &run_formatter!(cond_expr(expr()), "cond { a => b, { true } => d, else => f, }"),
-    //     expect_test::expect![[r#"cond { a => b, { true } => d, else => f, }"#]],
-    // );
+    check(
+        &run_formatter!(
+            cond_expr(expr()),
+            "cond { 
+                a           =>{ 
+                    b  },else => c,  }"
+        ),
+        expect_test::expect![[r#"
+        cond { a => {
+            b
+        }, else => c, }"#]],
+    );
+    check(
+        &run_formatter!(cond_expr(expr()), "cond {a => b,{true}=>d,else=>f,}"),
+        expect_test::expect![[r#"
+        cond { a => b, {
+            true
+        } => d, else => f, }"#]],
+    );
 }
