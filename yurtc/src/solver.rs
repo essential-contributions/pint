1c4d2e60810cebcc9bc1db539261383c6317d57d
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2023-12-21 16:02:16 -0500

    Support multiplication, division, and subtraction

--- yurtc/src/solver.rs
+++ yurtc/src/solver.rs
@@ -9,7 +9,7 @@ use interval::{interval_set::IntervalSet, ops::Range};
 use pcp::{
     concept::Var,
     kernel::Snapshot,
-    propagators::{x_geq_y, x_greater_y, x_leq_y, XEqY, XLessY, XNeqY},
+    propagators::{x_geq_y, x_greater_y, x_leq_y, XEqY, XEqYMulZ, XEqYPlusZ, XLessY, XNeqY},
     search::{
         one_solution_engine,
         search_tree_visitor::Status::{EndOfSearch, Satisfiable, Unsatisfiable},
@@ -31,16 +31,28 @@ pub struct Solver<'a> {
     variables: Vec<SolverVar>,
     intent: &'a Intent,
     status: Option<Status<FDSpace>>,
+    unique_var_suffix: usize, // unique suffix for variables introduced by the solver
+    dom_lower: i32,           // Default lower bound for decision variables
+    dom_upper: i32,           // Default upper bound for decision variables
 }
 
 impl<'a> Solver<'a> {
-    /// Creates a new instance of `Solver` given an `Intent`. Default initializes everything.
+    /// Creates a new instance of `Solver` given an `Intent`.
     pub fn new(intent: &'a Intent) -> Solver {
         Solver {
             space: FDSpace::empty(),
             variables: Vec::new(),
             intent,
             status: None,
+            unique_var_suffix: 0,
+
+            // TODO: The bounds chosen below are a temporary hack. One option is to use the largest
+            // possible interval: (i32::MIN, i32::MAX), but that causes some arithmetic overflow
+            // issues for some problems. Alternatively, an analysis of the program can lead to
+            // better bounds. This needs fixing eventually, but is probably fine for now while we
+            // develop the rest of the compiler.
+            dom_lower: -1_000_000,
+            dom_upper: 1_000_000,
         }
     }
 
@@ -68,6 +80,66 @@ impl<'a> Solver<'a> {
                 let rhs = self.expr_to_vstore(rhs)?;
                 match op {
                     expr::BinaryOp::Add => Ok(Box::new(Sum::new(vec![lhs, rhs]))),
+                    expr::BinaryOp::Sub => {
+                        // Introduce a new variable with a unique name, constrain it with `lhs ==
+                        // rhs + <new_var>`, and then return it.
+                        let sub_var = Box::new(
+                            self.space
+                                .vstore
+                                .alloc(IntervalSet::new(self.dom_lower, self.dom_upper)),
+                        ) as Var<VStore>;
+                        self.space.cstore.alloc(Box::new(XEqYPlusZ::new(
+                            lhs,
+                            rhs,
+                            sub_var.bclone(),
+                        )));
+                        self.variables.push(SolverVar {
+                            name: format!("INTRODUCED{}", self.unique_var_suffix),
+                            var: sub_var.bclone(),
+                        });
+                        self.unique_var_suffix += 1;
+                        Ok(sub_var)
+                    }
+                    expr::BinaryOp::Mul => {
+                        // Introduce a new variable with a unique name, constrain it to be equal to
+                        // `lhs * rhs`, and then return it.
+                        let mul_var = Box::new(
+                            self.space
+                                .vstore
+                                .alloc(IntervalSet::new(self.dom_lower, self.dom_upper)),
+                        ) as Var<VStore>;
+                        self.space.cstore.alloc(Box::new(XEqYMulZ::new(
+                            mul_var.bclone(),
+                            lhs,
+                            rhs,
+                        )));
+                        self.variables.push(SolverVar {
+                            name: format!("INTRODUCED{}", self.unique_var_suffix),
+                            var: mul_var.bclone(),
+                        });
+                        self.unique_var_suffix += 1;
+                        Ok(mul_var)
+                    }
+                    expr::BinaryOp::Div => {
+                        // Introduce a new variable with a unique name, constrain it with
+                        // `lhs == rhs * <new_var> `, and then return it.
+                        let div_var = Box::new(
+                            self.space
+                                .vstore
+                                .alloc(IntervalSet::new(self.dom_lower, self.dom_upper)),
+                        ) as Var<VStore>;
+                        self.space.cstore.alloc(Box::new(XEqYMulZ::new(
+                            lhs,
+                            rhs,
+                            div_var.bclone(),
+                        )));
+                        self.variables.push(SolverVar {
+                            name: format!("INTRODUCED{}", self.unique_var_suffix),
+                            var: div_var.bclone(),
+                        });
+                        self.unique_var_suffix += 1;
+                        Ok(div_var)
+                    }
                     _ => Err(SolveError::Internal {
                         msg: "(pcp) only addition is expected in expressions",
                         span: empty_span(),
@@ -111,16 +183,12 @@ impl<'a> Solver<'a> {
 
         // Convert all variables
         for v in &self.intent.vars {
-            // TODO: The bounds chosen below are a temporary hack. One option is to use the largest
-            // possible interval: (i32::MIN, i32::MAX), but that causes some arithmetic overflow
-            // issues for some problems. Alternatively, an analysis of the program can lead to
-            // better bounds.
             self.variables.push(SolverVar {
                 name: v.name.clone(),
                 var: Box::new(
                     self.space
                         .vstore
-                        .alloc(IntervalSet::new(-1_000_000_000, 1_000_000_000)),
+                        .alloc(IntervalSet::new(self.dom_lower, self.dom_upper)),
                 ) as Var<VStore>,
             });
         }
@@ -178,7 +246,7 @@ impl<'a> Solver<'a> {
     }
 
     /// Pretty print the output of the solver which includes a valid solution for all the variables
-    /// in case of success.
+    /// in case of success. Skips any helper variables introduced by the solver.
     pub fn print_solution(&self) {
         match self.status {
             Some(Satisfiable) => {
@@ -190,7 +258,12 @@ impl<'a> Solver<'a> {
                 );
                 println!("    {}", Style::new(Color::Green).bold().paint("Solution:"));
                 for (idx, dom) in self.space.vstore.iter().enumerate() {
-                    println!("     {}: {}", self.intent.vars[idx].name, dom.lower());
+                    // This assumes that the original variables are always added first, followed by
+                    // the list of helper variables introduced by the solver. Hence, only print the
+                    // first `self.intent.vars.len()` variables.
+                    if idx < self.intent.vars.len() {
+                        println!("     {}: {}", self.variables[idx].name, dom.lower());
+                    }
                 }
             }
             Some(Unsatisfiable) => println!(
@@ -211,7 +284,7 @@ impl<'a> Solver<'a> {
     }
 
     /// Serialize the raw output of the solver into a `String`. This is mostly meant for testing
-    /// purposes.
+    /// purposes. Includes helper variables introduced by the solver.
     pub fn display_solution_raw(&self) -> String {
         match self.status {
             Some(Satisfiable) => {
@@ -220,7 +293,7 @@ impl<'a> Solver<'a> {
                     .iter()
                     .enumerate()
                     .fold(String::new(), |mut acc, (idx, dom)| {
-                        writeln!(&mut acc, "{}: {}", self.intent.vars[idx].name, dom.lower())
+                        writeln!(&mut acc, "{}: {}", self.variables[idx].name, dom.lower())
                             .expect("Failed to write solution to string");
                         acc
                     })
