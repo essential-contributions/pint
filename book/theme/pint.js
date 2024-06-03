72b4ac61e94e00af0a32437993efa537f45e834d
mohammadfawaz <mohammadfawaz@users.noreply.github.com>
2024-06-03 23:14:06 +0000

    deploy: a46027db2c791a0e795c773c2cb49082edde0538

--- book/theme/pint.js
+++ book/theme/pint.js
@@ -2,7 +2,7 @@ hljs.registerLanguage("pint", (hljs) => ({
   name: "Pint",
   keywords: {
     keyword:
-      "real int bool string b256 forall exists fn if else cond let state storage extern constraint macro maximize minimize solve satisfy use self as enum intent in type where",
+      "real int bool string b256 forall exists fn if else cond let state storage interface constraint macro maximize minimize solve satisfy use self as enum intent in type where",
     literal: "false true",
   },
   contains: [
