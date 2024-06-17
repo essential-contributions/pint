4f5017a9755a1d215ff2adc8eec38e2ce86862d8
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-06-17 13:33:41 +1000

    feat: pint-pkg supports contract deps, includes a simple test
    This updates the `pint-pkg` build process to build a single small
    library for each built contract.
    
    This library is a small temporary directory with a `lib.pnt` entry point
    containing the `ADDRESS` for the contract, along with a submodule and
    `ADDRESS` for each intent.
    
    E.g. the counter app generates:
    
    ```
    <temp_dir>/<contract-content-address>/lib.pnt
    <temp_dir>/<contract-content-address>/Init.pnt
    <temp_dir>/<contract-content-address>/Increment.pnt
    ```
    
    where
    
    - `lib.pnt` contains the contract content address
    - `Init.pnt` is the `::Init` intent's content address
    - `Increment.pnt` is the `::Increment` intent's content address
    
    Currently we use `var` for these consts, but these should be updated
    when addressing #584.
    
    A test is included where a contract `foo` depends on a contract `bar`
    and uses `bar`'s addresses within some simple constraints. The test
    checks that `bar`'s CAs all appear within `foo`'s constraint bytecode.

--- pint-pkg/tests/build.rs
+++ pint-pkg/tests/build.rs
@@ -2,6 +2,7 @@
 
 #![allow(clippy::disallowed_names)]
 
+use essential_types::{ContentAddress, Word};
 use pint_pkg::{
     build::{build_plan, BuiltPkg},
     manifest::PackageKind,
@@ -32,7 +33,7 @@ fn build_default_library() {
 
 // Simple graph `foo` -> `bar`, i.e. foo depends on bar.
 #[test]
-fn build_contract_one_dep() {
+fn build_contract_one_lib_dep() {
     const BAR_SRC: &str = r#"type Age = int;"#;
     const FOO_SRC: &str = r#"use bar::Age;
 
@@ -80,12 +81,12 @@ solve satisfy;"#;
         };
 
         // There's only one nameless intent in our simple `foo` test contract.
-        assert_eq!(&contract.intents.names[0], "");
-        let intent = &contract.intents.intents[0];
+        assert_eq!(&contract.intents[0].name, "");
+        let intent = &contract.intents[0];
 
         // It should have at least one constraint.
         assert!(
-            !intent.constraints.is_empty(),
+            !intent.intent.constraints.is_empty(),
             "built foo should have at least one constraint"
         );
     });
@@ -161,13 +162,126 @@ solve satisfy;"#;
         };
 
         // There's only one nameless intent in our simple `foo` test contract.
-        assert_eq!(&contract.intents.names[0], "");
-        let intent = &contract.intents.intents[0];
+        assert_eq!(&contract.intents[0].name, "");
+        let intent = &contract.intents[0];
 
         // It should have at least one constraint.
         assert!(
-            !intent.constraints.is_empty(),
+            !intent.intent.constraints.is_empty(),
             "built foo should have at least one constraint"
         );
+    });
+}
+
+// Simple graph `foo` -> `bar`, i.e. foo depends on bar, both are contracts.
+#[test]
+fn build_contract_one_contract_dep() {
+    const BAR_SRC: &str = r#"storage {
+    counter: int,
+}
+
+intent Init {
+    var value: int;
+    state counter: int = storage::counter;
+    constraint counter' == value;
+}
+
+intent Increment {
+    state counter: int = storage::counter;
+    constraint counter' == counter + 1;
+}
+"#;
+
+    const FOO_SRC: &str = r#"
+// The top-level contract address.
+constraint bar::ADDRESS != 0x0000000000000000000000000000000000000000000000000000000000000000;
+
+// The intent addresses.
+constraint bar::Init::ADDRESS != 0x0000000000000000000000000000000000000000000000000000000000000000;
+constraint bar::Increment::ADDRESS != 0x0000000000000000000000000000000000000000000000000000000000000000;
+
+solve satisfy;"#;
+
+    with_temp_dir(|dir| {
+        // Overwrite `bar`'s source with a simple counter example.
+        let bar = new_pkg(&dir.join("bar"), PackageKind::Contract);
+        std::fs::write(bar.entry_point(), BAR_SRC.as_bytes()).unwrap();
+
+        // Overwrite `foo`'s source with our custom source that references `bar`'s CA.
+        let mut foo = new_pkg(&dir.join("foo"), PackageKind::Contract);
+        std::fs::write(foo.entry_point(), FOO_SRC.as_bytes()).unwrap();
+
+        // Create dependency foo -> bar.
+        edit_manifest(&mut foo, |m| insert_dep(m, &bar));
+
+        let members = [(foo.pkg.name.to_string(), foo)].into_iter().collect();
+        let plan = pint_pkg::plan::from_members(&members).unwrap();
+        let built = match build_plan(&plan) {
+            Ok(built) => built,
+            Err(err) => {
+                err.print();
+                panic!()
+            }
+        };
+
+        let order = plan.compilation_order();
+        assert_eq!(order.len(), 2, "expected two nodes in the plan");
+
+        // Check the first was bar and that it's a contract.
+        let bar_n = order[0];
+        let built_bar = &built.pkgs[&bar_n];
+        let BuiltPkg::Contract(bar_contract) = &built_bar else {
+            panic!("expected first built package `bar` to be a library");
+        };
+
+        fn intent_ca<'a>(
+            contract: &'a pint_pkg::build::BuiltContract,
+            name: &str,
+        ) -> &'a ContentAddress {
+            &contract
+                .intents
+                .iter()
+                .find(|i| i.name.contains(name))
+                .unwrap()
+                .ca
+        }
+
+        // Retrieve the CA hex strings.
+        let bar_ca = &bar_contract.ca;
+        let bar_init_ca = intent_ca(&bar_contract, "Init");
+        let bar_increment_ca = intent_ca(&bar_contract, "Increment");
+
+        // Check that the last was foo and that it's a contract.
+        let foo_n = order[1];
+        let built_foo = &built.pkgs[&foo_n];
+        let BuiltPkg::Contract(foo_contract) = built_foo else {
+            panic!("expected last built package `foo` to be a contract");
+        };
+
+        // Check that the bar contract and intent CAs appear in foo's constraints.
+        fn constraints_contain_ca(
+            contract: &pint_pkg::build::BuiltContract,
+            ca: &ContentAddress,
+        ) -> bool {
+            // Check one word-sized chunk of the CA at a time. We do this because
+            // in the bytecode, the CA is "pushed" to the stack one word at a time.
+            for ca_chunk in ca.0.chunks(std::mem::size_of::<Word>()) {
+                let mut contains_chunk = false;
+                for intent in &contract.intents {
+                    for constraint in &intent.intent.constraints {
+                        if constraint.windows(ca_chunk.len()).any(|w| w == ca_chunk) {
+                            contains_chunk |= true;
+                        }
+                    }
+                }
+                if !contains_chunk {
+                    return false;
+                }
+            }
+            true
+        }
+        assert!(constraints_contain_ca(&foo_contract, &bar_ca));
+        assert!(constraints_contain_ca(&foo_contract, &bar_init_ca));
+        assert!(constraints_contain_ca(&foo_contract, &bar_increment_ca));
     });
 }
