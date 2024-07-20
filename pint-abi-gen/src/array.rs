84a63518ae03a27a65535ca3931cfc6651251d04
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-07-20 17:27:14 +1000

    feat: Add pint-abi-gen Keys builder for building sets of keys
    The `Keys` builder is very similar to the `Mutations` builder, but just
    builds the keys rather than the key value pairs.

--- pint-abi-gen/src/array.rs
+++ pint-abi-gen/src/array.rs
@@ -1,16 +1,159 @@
 //! Items related to generating the `Array` mutations and keys builders.
 
 use crate::{
-    construct_key_expr, map, mutation, nesting_expr, nesting_key_doc_str, nesting_ty_str, tuple,
-    SingleKeyTy, TypeABI,
+    construct_key_expr, keys, map, mutations, nesting_expr, nesting_key_doc_str, nesting_ty_str,
+    tuple, SingleKeyTy, TypeABI,
 };
 use pint_abi_visit::{KeyedVarTree, Nesting, NodeIx};
 use proc_macro2::Span;
 
 /// The name for the array builder struct.
-pub(crate) fn mutations_struct_name(nesting: &[Nesting]) -> String {
+pub(crate) fn struct_name(nesting: &[Nesting]) -> String {
     format!("Array_{}", nesting_ty_str(nesting))
 }
+
+// ------------------------------------
+// Keys
+// ------------------------------------
+
+/// A builder struct for an array field.
+fn keys_struct(struct_name: &str, nesting: &[Nesting]) -> syn::ItemStruct {
+    let struct_ident = syn::Ident::new(struct_name, Span::call_site());
+    let nesting_key_doc_str = nesting_key_doc_str(nesting);
+    let doc_str = format!(
+        "A keys builder struct for the array at key `{nesting_key_doc_str}`.\n\n\
+        Generated solely for use within the `Keys` builder pattern.",
+    );
+    syn::parse_quote! {
+        #[doc = #doc_str]
+        #[allow(non_camel_case_types)]
+        pub struct #struct_ident<'a> {
+            keys: &'a mut Keys,
+        }
+    }
+}
+
+/// A map key builder method for entries with nested array values.
+fn key_method_for_array(arr_nesting: &[Nesting]) -> syn::ImplItemFn {
+    let struct_name = struct_name(arr_nesting);
+    let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
+    syn::parse_quote! {
+        /// Add keys for the nested array at the given key.
+        pub fn entry(mut self, ix: usize, f: impl FnOnce(#struct_ident) -> #struct_ident) -> Self {
+            self.key_elems.push(pint_abi::key::Elem::ArrayIx(ix));
+            f(#struct_ident { keys: &mut self.keys });
+            self.key_elems.pop();
+            self
+        }
+    }
+}
+
+/// An array key builder method for entries with nested map values.
+fn key_method_for_map(map_nesting: &[Nesting]) -> syn::ImplItemFn {
+    let struct_name = map::struct_name(map_nesting);
+    let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
+    syn::parse_quote! {
+        /// Add keys for the nested map at the given key.
+        pub fn entry(mut self, ix: usize, f: impl FnOnce(#struct_ident) -> #struct_ident) -> Self {
+            self.key_elems.push(pint_abi::key::Elem::ArrayIx(ix));
+            f(#struct_ident { keys: &mut self.keys });
+            self.key_elems.pop();
+            self
+        }
+    }
+}
+
+/// An array key builder method for entries with tuple values.
+fn key_method_for_tuple(tup_nesting: &[Nesting]) -> syn::ImplItemFn {
+    let struct_name = tuple::struct_name(tup_nesting);
+    let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
+    syn::parse_quote! {
+        /// Add keys for the tuple at the given index.
+        pub fn entry(mut self, ix: usize, f: impl FnOnce(#struct_ident) -> #struct_ident) -> Self {
+            self.key_elems.push(pint_abi::key::Elem::ArrayIx(ix));
+            f(#struct_ident { keys: &mut self.keys });
+            self.key_elems.pop();
+            self
+        }
+    }
+}
+
+/// An array key builder method for an element with a single-key.
+fn key_method_for_single_key(elem_nesting: &[Nesting]) -> syn::ImplItemFn {
+    let nesting_expr: syn::ExprArray = nesting_expr(elem_nesting);
+    let construct_key_expr: syn::Expr = construct_key_expr();
+    let nesting_key_doc_str = nesting_key_doc_str(elem_nesting);
+    let method_doc_str = format!(
+        "The given index will be flattened and encoded as a word and merged into the full key `{nesting_key_doc_str}`."
+    );
+    syn::parse_quote! {
+        /// Add a key for the element at the given index.
+        ///
+        #[doc = #method_doc_str]
+        pub fn entry(mut self, ix: usize) -> Self {
+            use pint_abi::types::essential::Key;
+            // Add the array index to the key stack.
+            self.key_elems.push(pint_abi::key::Elem::ArrayIx(ix));
+            // Merge the key stack with the ABI key.
+            let nesting = #nesting_expr;
+            let key: Key = #construct_key_expr;
+            // Add the key to the set.
+            self.set.retain(|k: &Key| k != &key);
+            self.set.push(key);
+            // Pop the entry key from the stack.
+            self.key_elems.pop();
+            self
+        }
+    }
+}
+
+/// An array method for inserting keys for an entry associated with a given index.
+fn key_method(tree: &KeyedVarTree, elem: NodeIx, elem_ty: &TypeABI) -> syn::ImplItemFn {
+    let elem_nesting = tree.nesting(elem);
+    match elem_ty {
+        TypeABI::Bool | TypeABI::Int | TypeABI::Real | TypeABI::String | TypeABI::B256 => (),
+        TypeABI::Array { .. } => return key_method_for_array(&elem_nesting),
+        TypeABI::Tuple { .. } => {
+            return key_method_for_tuple(&elem_nesting);
+        }
+        TypeABI::Map { .. } => {
+            return key_method_for_map(&elem_nesting);
+        }
+    };
+    key_method_for_single_key(&elem_nesting)
+}
+
+/// The implementation for the map keys builder of the given name.
+fn keys_impl(tree: &KeyedVarTree, array: NodeIx, struct_name: &str, ty: &TypeABI) -> syn::ItemImpl {
+    let struct_ident = syn::Ident::new(struct_name, Span::call_site());
+    // The node for an array elem is its only child.
+    let elem = tree.children(array)[0];
+    let method = key_method(tree, elem, ty);
+    syn::parse_quote! {
+        impl<'a> #struct_ident<'a> {
+            #method
+        }
+    }
+}
+
+/// Array builder types and impls for a given keyed array type.
+pub(crate) fn keys_items(tree: &KeyedVarTree, array: NodeIx, ty: &TypeABI) -> Vec<syn::Item> {
+    let nesting = tree.nesting(array);
+    let struct_name = struct_name(&nesting);
+    let mut items = vec![];
+    items.push(keys_struct(&struct_name, &nesting).into());
+    items.push(keys_impl(tree, array, &struct_name, ty).into());
+    items.extend(
+        keys::impl_deref_for_nested(&struct_name)
+            .into_iter()
+            .map(syn::Item::from),
+    );
+    items
+}
+
+// ------------------------------------
+// Mutations
+// ------------------------------------
 
 /// A builder struct for an array field.
 fn mutations_struct(struct_name: &str, nesting: &[Nesting]) -> syn::ItemStruct {
@@ -31,7 +174,7 @@ fn mutations_struct(struct_name: &str, nesting: &[Nesting]) -> syn::ItemStruct {
 
 /// A map mutation builder method for entries with nested array values.
 fn mutation_method_for_array(arr_nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = mutations_struct_name(arr_nesting);
+    let struct_name = struct_name(arr_nesting);
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     syn::parse_quote! {
         /// Add mutations for the nested array at the given key.
@@ -46,7 +189,7 @@ fn mutation_method_for_array(arr_nesting: &[Nesting]) -> syn::ImplItemFn {
 
 /// An array mutation builder method for entries with nested map values.
 fn mutation_method_for_map(map_nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = map::mutations_struct_name(map_nesting);
+    let struct_name = map::struct_name(map_nesting);
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     syn::parse_quote! {
         /// Add mutations for the nested map at the given key.
@@ -61,7 +204,7 @@ fn mutation_method_for_map(map_nesting: &[Nesting]) -> syn::ImplItemFn {
 
 /// An array mutation builder method for entries with tuple values.
 fn mutation_method_for_tuple(tup_nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = tuple::mutations_struct_name(tup_nesting);
+    let struct_name = tuple::struct_name(tup_nesting);
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     syn::parse_quote! {
         /// Add mutations for the tuple at the given index.
@@ -148,14 +291,14 @@ fn mutations_impl(
 }
 
 /// Array builder types and impls for a given keyed array type.
-pub(crate) fn builder_items(tree: &KeyedVarTree, array: NodeIx, ty: &TypeABI) -> Vec<syn::Item> {
+pub(crate) fn mutations_items(tree: &KeyedVarTree, array: NodeIx, ty: &TypeABI) -> Vec<syn::Item> {
     let nesting = tree.nesting(array);
-    let struct_name = mutations_struct_name(&nesting);
+    let struct_name = struct_name(&nesting);
     let mut items = vec![];
     items.push(mutations_struct(&struct_name, &nesting).into());
     items.push(mutations_impl(tree, array, &struct_name, ty).into());
     items.extend(
-        mutation::impl_deref_for_nested(&struct_name)
+        mutations::impl_deref_for_nested(&struct_name)
             .into_iter()
             .map(syn::Item::from),
     );
--- pint-abi-gen/src/mutations.rs
+++ pint-abi-gen/src/mutations.rs
@@ -9,17 +9,17 @@ use proc_macro2::Span;
 
 /// Recursively traverse the given keyed var and create a builder struct and
 /// associated impl for each tuple, map and array.
-fn nested_builder_items_from_node(tree: &KeyedVarTree, n: NodeIx) -> Vec<syn::Item> {
+fn nested_items_from_node(tree: &KeyedVarTree, n: NodeIx) -> Vec<syn::Item> {
     let mut items = vec![];
     match tree[n].ty {
         TypeABI::Array { ty, size: _ } => {
-            items.extend(array::builder_items(tree, n, ty));
+            items.extend(array::mutations_items(tree, n, ty));
         }
         TypeABI::Map { ty_from, ty_to } => {
-            items.extend(map::builder_items(tree, n, ty_from, ty_to));
+            items.extend(map::mutations_items(tree, n, ty_from, ty_to));
         }
         TypeABI::Tuple(_fields) => {
-            items.extend(tuple::builder_items(tree, n));
+            items.extend(tuple::mutations_items(tree, n));
         }
         TypeABI::Bool | TypeABI::Int | TypeABI::Real | TypeABI::String | TypeABI::B256 => (),
     }
@@ -28,11 +28,11 @@ fn nested_builder_items_from_node(tree: &KeyedVarTree, n: NodeIx) -> Vec<syn::It
 
 /// Recursively traverse the given keyed vars and create a builder structs and
 /// impls for each tuple, map and array.
-fn nested_builder_items_from_keyed_vars(vars: &[VarABI]) -> Vec<syn::Item> {
+fn nested_items_from_keyed_vars(vars: &[VarABI]) -> Vec<syn::Item> {
     let mut items = vec![];
     let tree = KeyedVarTree::from_keyed_vars(vars);
     tree.dfs(|n| {
-        items.extend(nested_builder_items_from_node(&tree, n));
+        items.extend(nested_items_from_node(&tree, n));
     });
     items
 }
@@ -58,7 +58,7 @@ fn method_doc_str(name: &str) -> String {
 
 /// A `Mutations` builder method for an array field.
 fn method_for_array(name: &str, array_nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = array::mutations_struct_name(array_nesting);
+    let struct_name = array::struct_name(array_nesting);
     let method_ident = syn::Ident::new(name, Span::call_site());
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     let doc_str = method_doc_str(name);
@@ -73,7 +73,7 @@ fn method_for_array(name: &str, array_nesting: &[Nesting]) -> syn::ImplItemFn {
 
 /// A `Mutations` builder method for a map field.
 fn method_for_map(name: &str, map_nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = map::mutations_struct_name(map_nesting);
+    let struct_name = map::struct_name(map_nesting);
     let method_ident = syn::Ident::new(name, Span::call_site());
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     let doc_str = method_doc_str(name);
@@ -88,7 +88,7 @@ fn method_for_map(name: &str, map_nesting: &[Nesting]) -> syn::ImplItemFn {
 
 /// A `Mutations` builder method for a tuple field.
 fn method_for_tuple(name: &str, nesting: &[Nesting]) -> syn::ImplItemFn {
-    let struct_name = tuple::mutations_struct_name(nesting);
+    let struct_name = tuple::struct_name(nesting);
     let method_ident = syn::Ident::new(name, Span::call_site());
     let struct_ident = syn::Ident::new(&struct_name, Span::call_site());
     let doc_str = method_doc_str(name);
@@ -146,7 +146,6 @@ pub(crate) fn method_from_node(tree: &KeyedVarTree, n: NodeIx, name: &str) -> sy
             return method_for_map(name, &nesting);
         }
     };
-    // A mutation builder method for a single mutation.
     method_for_single_key(name, &arg_ty, &nesting)
 }
 
@@ -230,13 +229,26 @@ pub(crate) fn impl_deref_for_nested(struct_name: &str) -> Vec<syn::ItemImpl> {
 }
 
 /// All items for the `Mutations` type, nested mutations builder types and their impls.
-pub(crate) fn items(vars: &[VarABI]) -> Vec<syn::Item> {
+fn items(vars: &[VarABI]) -> Vec<syn::Item> {
     let mut items = vec![
         mutations_struct().into(),
         mutations_fn().into(),
         impl_mutations(vars).into(),
         impl_from_mutations_for_vec().into(),
     ];
-    items.extend(nested_builder_items_from_keyed_vars(vars));
+    items.extend(nested_items_from_keyed_vars(vars));
     items
+}
+
+/// A `mutations` module for all `Mutations`-related items.
+pub(crate) fn module(vars: &[VarABI]) -> syn::ItemMod {
+    let items = items(vars);
+    syn::parse_quote! {
+        pub mod mutations {
+            //! All items related to building a set of [`Mutations`].
+            #(
+                #items
+            )*
+        }
+    }
 }
