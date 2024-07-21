d0b04d14ba8299abc234a23afb3e097b03245188
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-07-21 15:21:36 +1000

    feat: Add support for decoding ABI types from `Word`s
    Adds `Read` and `Decode` traits (to complement `Write` and `Encode`) for
    decoding ABI-generated types from words.
    
    This will be useful when requesting certain kinds of state from the
    server, and decoding that state into useful representations generated
    by `pint-abi-gen`.

--- pint-abi/src/lib.rs
+++ pint-abi/src/lib.rs
@@ -11,19 +11,25 @@ pub use pint_abi_gen::from_file as gen_from_file;
 pub use pint_abi_gen::from_str as gen_from_str;
 
 #[doc(inline)]
+pub use decode::Decode;
+#[doc(inline)]
 pub use encode::Encode;
+#[doc(inline)]
+pub use read::Read;
 #[doc(inline)]
 pub use write::Write;
 
 use std::path::Path;
 use thiserror::Error;
 use types::{
-    essential::{contract::Contract, predicate::Predicate},
+    essential::{contract::Contract, predicate::Predicate, Word},
     ContractABI, PredicateABI,
 };
 
+pub mod decode;
 mod encode;
 pub mod key;
+mod read;
 mod write;
 
 /// Both Pint ABI and Essential protocol types.
@@ -48,12 +54,46 @@ pub enum FromPathError {
     Json(#[from] serde_json::Error),
 }
 
+/// Attempted to `Decode` an instance of a type from a slice containing more
+/// `Word`s than necessary.
+#[derive(Debug, Error)]
+#[error("invalid slice length: `{remaining}` words too many")]
+pub struct TooManyWords {
+    /// The number of words left remaining in the slice.
+    pub remaining: usize,
+}
+
+/// Any error that might occur during a call to `decode`.
+#[derive(Debug, Error)]
+pub enum DecodeError<E> {
+    /// Failed to decode the type.
+    #[error("{0}")]
+    Decode(E),
+    /// Invalid slice length. Slice contained more words than necessary.
+    #[error("{0}")]
+    TooManyWords(#[from] TooManyWords),
+}
+
 /// Shorthand for encoding the given value into a `Vec` of Essential `Word`s.
-pub fn encode<T: Encode>(t: &T) -> Vec<types::essential::Word> {
+pub fn encode<T: Encode>(t: &T) -> Vec<Word> {
     let mut v = vec![];
     t.encode(&mut v)
         .expect("encoding into `Vec<Word>` cannot error");
     v
+}
+
+/// Shorthand for decoding an instance of `T` from a slice of Essential `Word`s.
+///
+/// Note that dynamically sized types (like `Vec<T>`) are not supported, as the
+/// length cannot be known.
+pub fn decode<T: Decode>(mut words: &[Word]) -> Result<T, DecodeError<T::Error>> {
+    let cursor = &mut words;
+    let t = T::decode(cursor).map_err(|e| DecodeError::Decode(e))?;
+    if !cursor.is_empty() {
+        let remaining = cursor.len();
+        return Err(TooManyWords { remaining }.into());
+    }
+    Ok(t)
 }
 
 /// Shorthand for loading the [`ContractABI`] from a given ABI JSON file path.
