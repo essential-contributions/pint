75e7591950aeeef2ac834df51d841465087d0381
lethalgem <iwc123@gmail.com>
2024-09-25 14:48:29 -0400

    Simplify binary ops with consts

--- pintc/src/predicate/optimize.rs
+++ pintc/src/predicate/optimize.rs
@@ -1,5 +1,8 @@
 mod dead_code_elimination;
+mod expr_simplification;
+
 use dead_code_elimination::dead_code_elimination;
+use expr_simplification::simplify_exprs;
 
 use crate::error::Handler;
 
@@ -7,6 +10,8 @@ impl super::Contract {
     pub fn optimize(mut self, handler: &Handler) -> Self {
         // Remove dead code
         dead_code_elimination(handler, &mut self);
+
+        simplify_exprs(&mut self);
 
         self
     }
