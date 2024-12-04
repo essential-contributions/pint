0d6d0f79663d5b2adb3a11dfa10cb6d912b58641
lethalgem <iwc123@gmail.com>
2024-12-04 15:36:16 -0500

    Add test for multiple predicate addresses

--- pintc/tests/cli.rs
+++ pintc/tests/cli.rs
@@ -258,3 +258,44 @@ fn explicit_salt_err() {
     );
     check(&output.stdout, expect_test::expect![""]);
 }
+
+#[test]
+fn multiple_predicate_output() {
+    let mut input_file = tempfile::NamedTempFile::new().unwrap();
+    write!(
+        input_file.as_file_mut(),
+        "predicate test() {{}}
+        predicate check(x : int) {{constraint x == 0;}}
+        predicate verify(x : int) {{constraint x == 1;}}
+        predicate validate(x : int) {{constraint x == 2;}}
+        predicate assess(x : int) {{constraint x == 3;}}
+        predicate examine(x : int) {{constraint x == 4;}}
+        predicate inspect(x : int) {{constraint x == 5;}}
+        predicate evaluate(x : int) {{constraint x == 6;}}"
+    )
+    .unwrap();
+
+    let output = pintc_command(input_file.path().to_str().unwrap());
+
+    assert!(input_file.path().with_extension("json").exists());
+    let _ = fs::remove_file(input_file.path().with_extension("json"));
+
+    let input_file_name = input_file.path().file_name().unwrap().to_str().unwrap();
+    let mut expected_output_file = tempfile::NamedTempFile::new().unwrap();
+    let expected_output = format!("    contract {input_file_name}           2F807BAF77DBE874929689A2A4011FC8C4AB5499FA7544FF599524E4354B4957
+         ├── {input_file_name}::test     BA6595C5C75346E6C82BED0CE770D0758ADD1712163FCE45E38E5E8EAC6AA153
+         ├── {input_file_name}::check    D40185838803AB6CF672C5C01D70A93F47666990443F39A151CD244CB0B81F07
+         ├── {input_file_name}::verify   9C765AEC92FC1F9D6F712ED04AFBC00AB67FD549EBA12878610D05C86F549350
+         ├── {input_file_name}::validate CF9B52686DD22EFF2217FD8739AFC47ABFFCC55AC1F0A5745F9EFFF8443CF5CC
+         ├── {input_file_name}::assess   9BD346668B83A078F68F635F57CFABC551865BD447534F2CF6B0E9707A144E58
+         ├── {input_file_name}::examine  F8B346FB584A105E6161440581AA987CBC5E7AEAD5F75C7C66120EB18C6CDA4B
+         ├── {input_file_name}::inspect  1D4EF7D65DAD4F59395291107A49FD0EF5D3A772BA711D1CAA4A5C404626A200
+         └── {input_file_name}::evaluate FD36462040324A54DEAD7781ACC79487F481DFDC21006A1E9E0A8FBFB8AC6669\n");
+    write!(expected_output_file.as_file_mut(), "{}", expected_output).unwrap();
+
+    check(&output.stderr, expect_test::expect![""]);
+    check_file(
+        &output.stdout,
+        expect_test::expect_file![expected_output_file.path()],
+    );
+}
